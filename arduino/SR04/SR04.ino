f72c1510e45029ab9e4e1bd1b243a3d6a9eb0f9d
ilmaruk <ruggero.ferretti@grapeshot.co.uk>
2019-05-05 09:22:43 +0100

    Checking velocity and alerting (only log, for now).

---
+++ arduino/SR04/SR04.ino
@@ -0,0 +1,59 @@
+//www.elegoo.com
+//2016.12.08
+#include "SR04.h"
+
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+#define LED 9
+#define SAMPLING_INTERVAL 250
+#define SPEED_LIMIT 1.0 // m/s
+
+SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
+
+long previousDistance = -1;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+
+  sr04 = SR04(ECHO_PIN, TRIG_PIN);
+
+  delay(1000);
+}
+
+void loop() {
+  long currentDistance = sr04.Distance();
+  //logValue("Distance", currentDistance, "mm");
+
+  if (previousDistance != -1) {
+    double movement = previousDistance - currentDistance;
+    //logValue("Movement", movement, "mm");
+
+    double velocity = movement / SAMPLING_INTERVAL; // mm/ms = m/s
+    logValue("Velocity", velocity, "m/s");
+
+    if (velocity > SPEED_LIMIT) {
+      alarm(velocity);
+    }
+  }
+
+  previousDistance = currentDistance;
+
+  delay(SAMPLING_INTERVAL);
+}
+
+void logValue(const char *prefix, double value, const char *unit) {
+  Serial.print(prefix);
+  Serial.print(": ");
+  Serial.print(value);
+  Serial.print(" ");
+  Serial.println(unit);
+}
+
+void alarm(double velocity) {
+  Serial.print("Alarm, speeding: ");
+  Serial.print(velocity);
+  Serial.println(" m/s");
+
+  // TODO: notify RaspberryPi
+}
