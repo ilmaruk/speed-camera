9e72a3be04b0e07de62c88a16fb2551d1695fe82
ilmaruk <ruggero.ferretti@grapeshot.co.uk>
2019-05-20 12:33:17 +0100

    Reflecting traffic light colours on sense hat.

--- raspberrypi/speedcamera/app.py
+++ raspberrypi/speedcamera/app.py
@@ -7,30 +7,29 @@ import cv2
 
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+from sense_hat import SenseHat
 
 
-def main(arduino, camera, classifier):
+def main(arduino, camera, classifier, sense_hat):
     try:
         while True:
             line = arduino.readline()
-            velocity = process_line(line)
-            if velocity is None:
-                continue
-
-            image = take_photo(camera)
-            analyse_photo(image, classifier)
+            velocity = process_line(line, classifier, sense_hat)
     finally:
         arduino.close()
 
 
-def process_line(line):
-    if not line.startswith(b'Speeding'):
-        return None
+def process_line(line, classifier, sense_hat):
+    if line.startswith(b'Speeding'):
+        logging.info('Received speeding alert: {:s}'.format(str(line)))
+        _, velocity = line.split(b',')
+        image = take_photo(camera)
+        analyse_photo(image, classifier)
 
-    logging.info('Received speeding alert: {:s}'.format(str(line)))
-
-    _, velocity = line.split(b',')
-    return velocity
+    elif line.startswith(b'State'):
+        logging.info('Received state update: {:s}'.format(str(line)))
+        _, id, red, amber, green = line.split(b',')
+        set_sense_hat(sense_hat, red, amber, green)
 
 
 def take_photo(camera):
@@ -50,6 +49,26 @@ def analyse_photo(image, classifier):
     cv2.imwrite(image_path, image)
     logging.info('Store new image at {:s}'.format(image_path))
 
+
+def set_sense_hat(sense_hat, red, amber, green):
+    R = [255 * red, 0, 0]
+    A = [0, 0, 0]
+    G = [0, 0, 255 * green]
+
+    shape = [
+        R, R, R, R, R, R, R, R,
+        R, R, R, R, R, R, R, R,
+        R, R, R, R, R, R, R, R,
+        A, A, A, A, A, A, A, A,
+        A, A, A, A, A, A, A, A,
+        G, G, G, G, G, G, G, G,
+        G, G, G, G, G, G, G, G,
+        G, G, G, G, G, G, G, G,
+    ]
+
+    sense_hat.set_pixels(shape)
+
+
 if '__main__' == __name__:
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
@@ -62,4 +81,6 @@ if '__main__' == __name__:
 
     camera = PiCamera()
     camera.rotation = 180
-    main(serial.Serial('/dev/ttyACM0', 9600), camera, cv2.CascadeClassifier('/usr/local/share/opencv4/haarcascades/haarcascade_russian_plate_number.xml'))
+    main(serial.Serial('/dev/ttyACM0', 9600), camera,
+         cv2.CascadeClassifier('/usr/local/share/opencv4/haarcascades/haarcascade_russian_plate_number.xml'),
+         SenseHat())
